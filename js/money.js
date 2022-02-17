8ed60a6e48a4e3c4468a8048f274698192c5d1df
emonahmed123 <imonshomon@gmail.com>
2022-02-17 01:09:06 -0800

    my second added

---
+++ js/money.js
@@ -0,0 +1,14 @@
+document.getElementById('balance-button').addEventListener('click',function(){
+   const foodCostInput =document.getElementById('food-cost');
+    const foodCost   = parseFloat(foodCostInput.value);
+    
+
+      const rentCostInput = document.getElementById('Rent-cost');
+      const rentCost    =  parseFloat(foodCostInput.value);
+
+      const clocth
+
+
+
+
+})
